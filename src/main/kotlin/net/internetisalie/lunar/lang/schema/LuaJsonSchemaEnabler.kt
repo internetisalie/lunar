5f201050fd17b19c0aa0876bdf5a1db809f41927
Michael Crawford <internetisalie@gmail.com>
2026-06-28 22:26:53 -0400

    fix(schema): simplify enabler to avoid indexing interference

--- src/main/kotlin/net/internetisalie/lunar/lang/schema/LuaJsonSchemaEnabler.kt
+++ src/main/kotlin/net/internetisalie/lunar/lang/schema/LuaJsonSchemaEnabler.kt
@@ -3,22 +3,10 @@ package net.internetisalie.lunar.lang.schema
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.jsonSchema.extension.JsonSchemaEnabler
-import com.jetbrains.jsonSchema.ide.JsonSchemaService
 import net.internetisalie.lunar.lang.LuaFileType
 
 class LuaJsonSchemaEnabler : JsonSchemaEnabler {
-    private val isChecking = ThreadLocal.withInitial { false }
-
     override fun isEnabledForFile(file: VirtualFile, project: Project?): Boolean {
-        if (file.fileType != LuaFileType) return false
-        if (project == null) return false
-
-        if (isChecking.get()) return false
-        isChecking.set(true)
-        try {
-            return JsonSchemaService.Impl.get(project).getSchemaFilesForFile(file).isNotEmpty()
-        } finally {
-            isChecking.set(false)
-        }
+        return file.fileType == LuaFileType
     }
 }
